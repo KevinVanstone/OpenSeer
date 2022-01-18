adef99c87fba84f8c4bfd008601adb01787d4d37
KevinVanstone <40321708+KevinVanstone@users.noreply.github.com>
2022-01-18 11:28:03 -0800

    Home Links added - only text for now.

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import "./Home.scss";
+import { Link } from "react-router-dom";
+
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <div className="home">
+                    <Link to="/search">
                    <h1>Search</h1>
-                   <h1>Collections</h1> 
-                   <h1>Slideshow</h1> 
-                   <h1>About Open Seer</h1>  
+                   </Link>
+                   <Link to="/collections">
+                   <h1>Collections</h1>
+                   </Link> 
+                   <Link to="/slideshow">
+                   <h1>Slideshow</h1>
+                   </Link>
+                   <Link to="/about">
+                   <h1>About Open Seer</h1>
+                   </Link> 
                 </div>
             </div>
         );
