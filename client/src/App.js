96c22013219dec935ee83eb426172cabe5c3595a
KevinVanstone <40321708+KevinVanstone@users.noreply.github.com>
2022-01-18 11:14:21 -0800

    Basic homepage headers added.

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,11 @@ import Header from "./components/Header/Header.jsx";
 import Search from "./components/Search/Search.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import About from "./components/About/About.jsx";
+import Home from "./components/Home/Home.jsx";
 import "./App.css";
 import "./styles/partials/_globals.scss";
 import React from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -13,12 +14,12 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/search" element={<Search />} />
-        <Route path="/about" element={<About />} />
-        {/* <Route path="/collection" element={<Collection />} /> */}
-        {/* <Route path="/slideshow" element={<Slideshow />} /> */}
-        {/* <Route path="/profile" element={<Profile />} /> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/about" element={<About />} />
+          {/* <Route path="/collection" element={<Collection />} /> */}
+          {/* <Route path="/slideshow" element={<Slideshow />} /> */}
+          {/* <Route path="/profile" element={<Profile />} /> */}
         </Routes>
         <Footer />
       </BrowserRouter>
