79e24781d28b8ca1f36515686b933ea77d73894b
KevinVanstone <40321708+KevinVanstone@users.noreply.github.com>
2022-01-30 11:50:06 -0800

    About page QR

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import "./About.scss";
-
+import QR from "../../assets/images/contactQR.png";
 
 class About extends Component {
   render() {
     return <div className="about">
-    <h1>About Open Seer</h1>
+    <h1 className="about__hed">About Open Seer</h1>
     <p>Open Seer is a blockchain explorer tool that allows users to
 search the Ethereum network for any wallet address or text string,
 returning the associated data. Any valid ETH wallet address will
@@ -13,6 +13,8 @@ return the NFTs within that wallet. Any other text string will
 return a search of that string across assets on the Ethereum network
 as provided by the NFT Port API. 
 </p>
+<img className="about__qr" src={QR} alt="Contact Kevin Vanstone" />
+
     </div>;
   }
 }
